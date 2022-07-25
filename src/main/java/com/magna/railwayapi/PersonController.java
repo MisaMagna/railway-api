45c2fc1a45fe2fd2854dd4c142c06a2d3284914f
Misael Pereira <misamagna@gmail.com>
2022-07-25 23:11:47 +0200

    Initial Commit

---
+++ src/main/java/com/magna/railwayapi/PersonController.java
@@ -0,0 +1,49 @@
+package com.magna.railwayapi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PersonController {
+
+    @Value("${my.environment.property}")
+    private String environmentVar;
+
+    private List<Person> persons;
+
+    @PostConstruct
+    public void init() {
+        persons = new ArrayList<>();
+        persons.add(new Person("Misa", 27));
+        persons.add(new Person("Tom", 26));
+        persons.add(new Person("Clau", 15));
+    }
+
+    @GetMapping("/environment")
+    public String getEnvironment() {
+        return environmentVar;
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    @GetMapping("/persons/{name}")
+    public Optional<Person> getPerson(@PathVariable String name) {
+        return persons.stream()
+                .filter(person -> person.getName().equalsIgnoreCase(name))
+                .findFirst();
+    }
+
+    @PostMapping("/persons")
+    public Person createPerson(@RequestBody Person person) {
+        persons.add(person);
+        return person;
+    }
+}
---
+++ src/test/java/com/magna/railwayapi/RailwayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magna.railwayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
